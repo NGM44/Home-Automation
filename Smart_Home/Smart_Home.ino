a3489ec5f44a3f547e6b98f5b626f30d11090ec4
NGM44 <48880215+NGM44@users.noreply.github.com>
2020-10-04 17:58:33 +0530

    Update Smart_Home.ino

--- Smart_Home/Smart_Home.ino
+++ Smart_Home/Smart_Home.ino
@@ -1,6 +1,10 @@
+/* Nikhil G Mudakavi */
+/*nikhilgmudakavi@gmail.com*/
+/*program to control Light , irrigation , water tank*/
+
 int ldr=A0;
 int soil=A1;
-int pir=4;
+int pir=A2;
 int motor=8;
 int relay=13;
 int led=12;
@@ -9,12 +13,14 @@ int motorvalue=0;
 int ldrvalue=0;
 int pirvalue=0;
 int soilvalue=0;
-byte val=0;
+byte val;
 void auto_function();
 void main_function();
-void setup() {
-  //setup code starts here
-  Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);//Change the baud rate value depending on the default baud rate of your bluetooth module, for Bluesmirf-115200 and for JY-MCU-9600
+  
+ Serial.begin(9600);
   pinMode(ldr,INPUT); 
   pinMode(soil,INPUT);
   pinMode(pir,INPUT);
@@ -25,40 +31,35 @@ void setup() {
 }
 
 void main_function(){
-  if(int(val)==49 ||(ldrvalue>100 && pirvalue==HIGH))//Turn Light ON
+  if(int(val)==49 ||(ldrvalue>100 && pirvalue>200))//Turn Light ON
         digitalWrite(led,HIGH);
-else if (int(val)==50 ||(ldrvalue>100 && pirvalue==HIGH))//Turn Light OFF
+else if (int(val)==50 ||(ldrvalue<100 && pirvalue<200))//Turn Light OFF
          digitalWrite(led,LOW);
 
 if(int(val)==51 || soilvalue>800)//Turn on soil moter
    digitalWrite(relay,HIGH);
- else if(int(val)==52 || soilvalue>800)//Turn soil moter OFF
+ else if(int(val)==52 || soilvalue<800)//Turn soil moter OFF
       digitalWrite(relay,LOW);
       
 if(int(val)==53 || motorvalue==HIGH)//Turn Water Moter ON
    digitalWrite(motorout,HIGH);
- else if(int(val)==54 || motorvalue==HIGH)//Turn Light3 OFF
+ else if(int(val)==54 || motorvalue==LOW)//Turn Light3 OFF
        digitalWrite(motorout,LOW);
 
 }
-
-void auto_function()
-{  ldrvalue=analogRead(ldr);
-  pirvalue=digitalRead(pir);
+void auto_function(){  ldrvalue=analogRead(ldr);
+  pirvalue=analogRead(pir);
   soilvalue=analogRead(soil);
   motorvalue=digitalRead(motor);
   main_function();
 }
-
-
-void loop() {
-  // main Code
- if(Serial.available())
+void loop()
+{
+   if(Serial.available())
   {
     val=Serial.read();
     Serial.println(int(val));//Display received value on Serial Monitor
     main_function();
   }
-  auto_function();
-  }
-  
+auto_function();
+}
